33c49eed915f083034bc4ff664059df8c2acffe0
Soumyajyoti Banerjee <banerjee.soumyajyoti4@gmail.com>
2017-08-13 21:45:33 +0530

    word2vec model prepeare and save

---
+++ scripts/script_model_word2vec.py
@@ -0,0 +1,14 @@
+
+
+
+
+from gensim.models.word2vec import LineSentence
+ from gensim.models import Word2Vec
+import pickle
+
+ sentences = LineSentence('text8')
+ model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+fname = open('save_model','w')
+op = open('store_model','wb')
+pickle.dump(model,op)
+ model2 = pickle.load(open('store_model','rb'))
