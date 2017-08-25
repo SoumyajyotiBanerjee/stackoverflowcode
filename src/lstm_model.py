d7edf376189f0044fbdad10d180b0ec201610724
Deep Learning <banerjee.soumyajyoti4@gmail.com>
2017-08-24 23:50:18 -0700

    new branch

--- src/lstm_model.py
+++ src/lstm_model.py
@@ -12,7 +12,7 @@ import pickle
 #Load Data
 d2v_model = doc2vec.Doc2Vec.load('../data/d2v_set_20k')
 x_train = np.array([e for e in d2v_model.docvecs], dtype='float')
-#with open('/home/deeplearning/stackoverflow/part2/tag_reco_data/data/tag_vec_set_1k', 'rb') as fp:
+#with open('../data/tag_vec_set_1k', 'rb') as fp:
 #    y_train = pickle.load(fp)
 fp = open('../data/tag_vec_set_20k', 'rb')
 y_train = pickle.load(fp)
@@ -23,6 +23,7 @@ x_train = x_train.reshape(x_train.shape[0], 1, x_train.shape[1])
 x_test = x_test.reshape(x_test.shape[0], 1, x_test.shape[1])
 
 #Build model
+
 model = Sequential()
 model.add(InputLayer(input_shape=(x_train.shape[1], x_train.shape[2])))
 model.add(LSTM(2000,dropout=0.1),activation='relu')
@@ -38,7 +39,7 @@ model.fit(x_train, y_train, epochs=10, batch_size=2000)
 
 #Save serialized model
 model_yaml = model.to_yaml()
-with open('/home/deeplearning/stackoverflow/part2/tag_reco_data/data/model_set_20k.yaml', 'w') as mfp:
+with open('../data/model_set_20k.yaml', 'w') as mfp:
     mfp.write(model_yaml)
 model.save_weights('/home/deeplearning/stackoverflow/part2/tag_reco_data/data/new_model_set_20k.h5')
 
